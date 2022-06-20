456535fc96158a04642b73ea9e6132957979ba29
lealgabrielp@gmail.com <lealgabrielp@gmail.com>
2022-06-19 22:12:37 -0300

    Added new view and url

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request: object) -> HttpResponse:
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request: object, id: int) -> HttpResponse:
+    return render(request, 'recipes/pages/home.html')
