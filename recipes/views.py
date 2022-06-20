b298587613fb408937161284b296a47dbda55459
lealgabrielp@gmail.com <lealgabrielp@gmail.com>
2022-06-20 00:13:14 -0300

    Injeting data in the home view list

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request: object) -> HttpResponse:
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request: object, id: int) -> HttpResponse:
-    return render(request, 'recipes/pages/recipe_view.html')
+    return render(request, 'recipes/pages/recipe_view.html', context={
+        'recipe': make_recipe(),
+    })
