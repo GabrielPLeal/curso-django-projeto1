904e6808e0fe8086e74efc8ce587740286b8ba80
Gabriel Leal <gabriel.leal@nexxera.com>
2022-07-06 19:17:00 -0300

    Testing no recipes found

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -6,9 +6,7 @@ from recipes import views
 class RecipeViewsTest(TestCase):
 
     def test_recipe_home_view_function_is_corretct(self):
-        view = resolve(
-            reverse('recipes:home')
-            )
+        view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
     def test_recipe_category_view_function_is_corretct(self):
@@ -24,13 +22,16 @@ class RecipeViewsTest(TestCase):
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_home_view_return_status_code_200_ok(self):
-        response = self.client.get(
-            reverse('recipes:home')
-            )
+        response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
-    
+ 
     def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(
-            reverse('recipes:home')
-            )
+        response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipe(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            ' <h1>No recipes found here.</h1>',
+            response.content.decode('utf-8')
+            )
