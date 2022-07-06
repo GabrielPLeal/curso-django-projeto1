6d87e7bce66bcfdb0cf2c7e7ed694a1ac6fc118e
Gabriel Leal <gabriel.leal@nexxera.com>
2022-07-06 19:10:51 -0300

    Use self.client  from django to simulate client

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -6,13 +6,31 @@ from recipes import views
 class RecipeViewsTest(TestCase):
 
     def test_recipe_home_view_function_is_corretct(self):
-        view = resolve(reverse('recipes:home'))
+        view = resolve(
+            reverse('recipes:home')
+            )
         self.assertIs(view.func, views.home)
 
     def test_recipe_category_view_function_is_corretct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+            )
         self.assertIs(view.func, views.category)
 
     def test_recipe_detail_view_function_is_corretct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+            )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_return_status_code_200_ok(self):
+        response = self.client.get(
+            reverse('recipes:home')
+            )
+        self.assertEqual(response.status_code, 200)
+    
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(
+            reverse('recipes:home')
+            )
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
